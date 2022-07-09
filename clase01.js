05d3cbe598a5673cac7cd00f31e5c07f46c39285
masterdf159 <j-s-tp@hotmail.com>
2022-07-09 08:38:38 -0500

    Update clase01.js

--- clase01.js
+++ clase01.js
@@ -1,7 +1,9 @@
 
 var x = myFunction(4, 3);
-document.getElementById("multi").innerHTML = x;
+document.getElementById("clase01.hmtl/multi").innerHTML = x;
 
 function myFunction(a, b) {
   return a * b;
 }
+
+console.log(x)

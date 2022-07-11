5a82f2a7a21b9a323fa3cfae328b8430415cf39a
masterdf159 <j-s-tp@hotmail.com>
2022-07-10 22:33:17 -0500

    s
    ds

--- ejercicios.js
+++ ejercicios.js
@@ -7,16 +7,6 @@ console.log(meses[0])
 
 }
 
-newFunction_text()
 
-function newFunction_text()
-{
-var valores=[true, 5, false, "hola" , "adios" , 2]
-const longitudtext = text.length < [valores]
-if(longitudtext > valores){
-    console.log(longitudtext, 'valores')
-}else{
 
-}
 
-}

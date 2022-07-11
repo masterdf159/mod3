b60a012c744bc9a6ae6ac912ceacc699cc102b4b
masterdf159 <j-s-tp@hotmail.com>
2022-07-10 21:47:18 -0500

    g
    sds

--- ejercicios.js
+++ ejercicios.js
@@ -1,3 +1,8 @@
+eje1()
+
+function eje1(){
 
 const meses = ["enero", "febrero", "marzo" , "abril" , "mayo" , "junio" , "julio" , "agosto" , "septiembre" , "octube" , "noviembre" , "diciembre"]
 console.log(meses[0])
+
+}

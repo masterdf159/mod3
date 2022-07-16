1dbb6a87a089664fb18155b9c451dfbc5feaec52
masterdf159 <j-s-tp@hotmail.com>
2022-07-16 08:15:26 -0500

    Update clase02.js

--- clase02.js
+++ clase02.js
@@ -1,6 +1,12 @@
 const name = ['pepe' , 'marina']
-console.log(name)
+/* console.log(name)
 name.forEach((valor, index) => {
     console.log(valor, index)
     
-})
+}) */
+
+//FOR
+
+for(let i = 0; name.length > i; i++){
+    console.log(name[i])
+}

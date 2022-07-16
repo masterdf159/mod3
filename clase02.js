5fa7848ec6006b3478f5564bd4ed3a345eefe5e3
masterdf159 <j-s-tp@hotmail.com>
2022-07-16 08:36:00 -0500

    Update clase02.js

--- clase02.js
+++ clase02.js
@@ -1,12 +1,31 @@
-const name = ['pepe' , 'marina']
-/* console.log(name)
+/* const name = ['pepe' , 'marina']
+console.log(name)
 name.forEach((valor, index) => {
     console.log(valor, index)
     
-}) */
+}) 
 
 //FOR
 
-for(let i = 0; name.length > i; i++){
-    console.log(name[i])
+for(let i = 0; 100 > i; i++){
+    console.long(i)
+} */
+
+
+const total= 0
+const value = [
+    {num1: 12, num2: 222},
+    {num1: 3, num2: 11},
+    {num1: 33, num2: 44},
+    {num1: 66, num2: 99},
+    {num1: 555, num2: 888},
+];
+
+function values(){
+for(let i = 0; i < value.length; i++){
+    if(value[i]!=0){
+     total = total + (value[i]*total);
+      }
+}return total;
 }
+console.log(values());

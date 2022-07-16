0999792b325bdb6cbbea9ff85b7294322aebe97d
masterdf159 <j-s-tp@hotmail.com>
2022-07-16 08:59:08 -0500

    Update clase02.js

--- clase02.js
+++ clase02.js
@@ -13,15 +13,23 @@ for(let i = 0; 100 > i; i++){
 
 
 
-const value = [
-    {num1: 12, num2: 222},
-    {num1: 3, num2: 11},
-    {num1: 33, num2: 44},
-    {num1: 66, num2: 99},
-    {num1: 555, num2: 888},
-];
 
 
-for(let i = 0; i < value.length; i++){  
-     console.log('Multi' , value[i].num1*value[i.num2])
+
+function values(){
+    const value = [
+        {num1: 12, num2: 222},
+        {num1: 3, num2: 11},
+        {num1: 33, num2: 44},
+        {num1: 66, num2: 99},
+        {num1: 555, num2: 888},
+    ];
+
+
+for(let i = 0; i < value.length; i++){ 
+     suma = num1 + num2
+     multi = num1 * num2 
+     console.log( `La Suma de ${value[i].num1 + value[i].num2}`  , `La multi de ${value[i].num1 * value[i].num2}`)
 }
+}
+console.log(values())

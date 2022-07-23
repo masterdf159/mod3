83552427aa6e9a024c37b6dc6c3be8e838ba5aec
masterdf159 <j-s-tp@hotmail.com>
2022-07-23 08:25:13 -0500

    sdf
    dasf

--- js/Taller01.js
+++ js/Taller01.js
@@ -1,2 +1,16 @@
 
-    
+// Declaracion OP
+var operandoa;
+var operandob;
+var operacion;
+
+//Funcion IN
+function init(){
+    //variables
+    var convert = document.getElementById('convert');
+    var dolar = document.getElementById('dolar');
+    var euro = document.getElementById('euro');
+  }
+//Evento de seguimiento
+
+// Operacion

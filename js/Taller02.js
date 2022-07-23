dc4284a603239e7680459f382b820b7436c900cc
masterdf159 <j-s-tp@hotmail.com>
2022-07-23 09:36:41 -0500

    ds
    fd

--- js/Taller02.js
+++
@@ -1,6 +0,0 @@
-function pesodolar(valNum) {
-    document.getElementById("pesodolar").value=valNum/4,408.03;
-  }
-  function pesoeuro(valNum) {
-    document.getElementById("pesoeuro").value=valNum/4,502.58;
-  }

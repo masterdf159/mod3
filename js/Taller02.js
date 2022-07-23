e8ef2ba359baa69b9741b28d4d38094eff093084
masterdf159 <j-s-tp@hotmail.com>
2022-07-23 11:26:59 -0500

    fdf
    df

--- js/Taller02.js
+++ js/Taller02.js
@@ -1,17 +1,17 @@
 
 function convertir(){
-    var valore = parseInt(document.getElementById("valorC").value)
+    var valore = parseInt(document.getElementById("valor").value)
     var resultado = 0
     var dolar = 4408
     var euro = 4501
-    if(document.getElementById("USD").checked){
+    if(document.getElementById("usd").checked){
         resultado = valore / dolar;
-        alert("EL cambio de Pesos Colombianos a Dolares es:" + resultado)
+        alert("EL cambio de Pesos Colombianos a Dolares es:" + resultado-toFixed(2))
     }
-    else if(document.getElementById("EU").checked)
+    else if(document.getElementById("eu").checked)
     {
         resultado = valore / euro;
-        alert("EL cambio de Pesos Colombianos a Euros es:" + euro)
+        alert("EL cambio de Pesos Colombianos a Euros es:" + euro.toFixed(2))
     }
     else{
         alert("Tienes que completar todos los campos requeridos.")

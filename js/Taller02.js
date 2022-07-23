4fea6c3a1cd2bc659fc39f060516d9c68bf8b5cd
masterdf159 <j-s-tp@hotmail.com>
2022-07-23 12:14:20 -0500

    df
    ssdf

--- js/Taller02.js
+++ js/Taller02.js
@@ -1,15 +1,18 @@
 
-function convertir(){
+alert('prueba')
+function convertir() {
+    console.log('entro')
     var valore = parseInt(document.getElementById("valor").value)
     var resultado = 0
     var dolar = 4408
     var euro = 4501
-    if(document.getElementById("usd").value == 'USD'){
+    console.log(document.getElementById("estilo2").value )
+    if(document.getElementById("estilo2").value == 'USD'){
         resultado = valore / dolar
         alert("EL cambio de Pesos Colombianos a Dolares es: $" + resultado.toFixed(2))
         
     }
-    else if(document.getElementById("eu").value == 'EU')
+    else if(document.getElementById("estilo2").value == 'EU')
     {
         resultado = valore / euro
         alert("EL cambio de Pesos Colombianos a Euros es: €" + resultado.toFixed(2))
@@ -17,6 +20,5 @@ function convertir(){
     }
     else{
         alert("Tienes que completar todos los campos requeridos.")
-       
     }
 }
